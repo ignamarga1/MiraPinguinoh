3c0a2b5054bbd7cd79c4879412dfe2aab5df7dca
Pablo Sánchez González <pablosgon2@gmail.com>
2026-01-31 13:37:39 +0100

    tango

--- Godot/mira-pinguinoh/scripts/interactable_object.gd
+++ Godot/mira-pinguinoh/scripts/interactable_object.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var is_interactable = true
+
 @export var i = 0
 @export var j = 0
 @export var indicator: Sprite2D
@@ -11,7 +13,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var interaction_pressed = Input.is_action_just_pressed("Interact")
-	if interaction_pressed && player_inside_interaction_area:
+	if interaction_pressed && player_inside_interaction_area && is_interactable:
 		SignalManager.player_interaction.emit(i, j)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
