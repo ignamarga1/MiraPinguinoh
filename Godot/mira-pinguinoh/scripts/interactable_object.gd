e2aaf24df291e0d9fe587231e2f5579d75463525
Pablo Sánchez González <pablosgon2@gmail.com>
2026-02-01 11:11:46 +0100

    visual changes

--- Godot/mira-pinguinoh/scripts/interactable_object.gd
+++ Godot/mira-pinguinoh/scripts/interactable_object.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export var i = 0
 @export var j = 0
 @export var indicator: Sprite2D
+@export var quote: Sprite2D
 
 var player_inside_interaction_area = false
 
@@ -18,12 +19,14 @@ func _process(delta: float) -> void:
 		SignalManager.player_interaction.emit(i, j)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
+	if body is Player && is_interactable:
 		player_inside_interaction_area = true
+		quote.visible = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is Player:
+	if body is Player && is_interactable:
 		player_inside_interaction_area = false
+		quote.visible = false
 
 func light_indicator(x: int, y: int, color: Color):
 	if x == i && y == j:
